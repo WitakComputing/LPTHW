376b302f2d44656e7e50281077b2591d3001488d
Konrad Witak - Disking IT Support KonradWitak-DiskingI Web Services <konrad@disking.co.uk>
2022-05-06 11:19:54 +0100

    Second Commit

---
+++ Exercises/ex1.py
@@ -0,0 +1,10 @@
+print("Hello World!") #First line
+#print("Hello Again")
+#print("I like typing this.")
+#print("This is fun.")
+#print('Yay! Printing')
+#print("I'd much rather you 'not'.")
+#print('I "said" do not touch this.')
+print("Cool story bro")
+
+#Complete
