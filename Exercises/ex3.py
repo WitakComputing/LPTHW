840a285322fe981b33a8d76839550ba6230a9627
Konrad Witak - Disking IT Support KonradWitak-DiskingI Web Services <konrad@disking.co.uk>
2022-05-06 12:46:05 +0100

    Third upload by myself

---
+++ Exercises/ex3.py
@@ -0,0 +1,24 @@
+# displays the line in the parentheses
+print("I will now count my chickens:")
+# prints the word, following by printing the calculation answer
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+# prints the string in the parentheses
+print("Now I will count the eggs:")
+# prints the calculation answer
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+# prints the string in parentheses
+print("Is it true that 3 + 2 < 5 - 7?")
+# prints the calculation answer
+print(3 + 2 < 5 - 7)
+# prints the string in the parentheses followed by the answer
+print("What is 3 + 2?", 3 + 2)
+print("What is 5 - 7?", 5 - 7)
+# prints the sentence
+print("Oh, that's why it's False.")
+# prints the sentence
+print("How about some more.")
+# prints the question followed by the comparison answer
+print("Is it greater?", 5 > -2) #True
+print("Is it greater or equal?", 5 >= -2) #True
+print("Is it less or equal?", 5 <= -2) #False
---
+++ Exercises/study_drill3.py
@@ -0,0 +1,5 @@
+print("what is 30+35?", 30 + 35)
+print("40+50 is",40 + 50)
+print("is 20 greater than 30?", 20 > 30)
+print("is 20 less than or equal to 30?", 20 <= 30) #False
+# Mini calculations from my study drill
