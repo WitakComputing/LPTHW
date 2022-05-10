05641c664236b5c26481bc270832577d30cabff3
Konrad Witak - Disking IT Support KonradWitak-DiskingI Web Services <konrad@disking.co.uk>
2022-05-10 11:13:24 +0100

    Money Converter

--- Exercises/Exercise 8/ex8.py
+++ Exercises/Exercise 8/ex8.py
@@ -5,8 +5,13 @@ print(formatter.format("one", "two", "three", "four"))
 print(formatter.format(True, False, False, True))
 print(formatter.format(formatter, formatter, formatter, formatter))
 print(formatter.format(
-    "Konrad is awesome",
-    "He's learning Python",
-    "He will be a programmer",
-    "He's the best"
+    "This is a money converter",
+    "First attempt",
+    "..",
+    "...."
 ))
+print ("USD to GBP money converter") #Mini money converter with Robins assist
+
+formatter2 = "{Price:.2f} Pounds" #a variable which formats the value inputted to 2 decimal places
+
+print(formatter2.format(Price=float(input("Please enter the amount in USD: "))/1.2322)) #prints the input and then multiplies the answer entered with
