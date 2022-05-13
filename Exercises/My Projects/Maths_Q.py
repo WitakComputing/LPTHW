3a2d6e289ff2ae82fee0780d4fcd0a569bc23561
Konrad Witak - Disking IT Support KonradWitak-DiskingI Web Services <konrad@disking.co.uk>
2022-05-13 16:44:53 +0100

    Random list

--- Exercises/My Projects/Maths_Q.py
+++ Exercises/My Projects/Maths_Q.py
@@ -1,4 +1,5 @@
 #Maths Questionnaire Game with the help of Robin
+import random
 print ("Welcome to the maths questionnaire")
 username = input("Please enter your username:")
 print("Welcome " + username)
@@ -12,15 +13,22 @@ def question(question, answer, current_count):
         print("Incorrect")
    # print (f"Your score is: {current_count}")
     return current_count
-
-count = question("1. What's 3x3?", "9", count)
-count = question("2. What's 9x4?", "36", count)  
-count = question("3. What's 6x6?", "36", count) 
-count = question("4. What's 7x7?", "49", count) 
-count = question("5. What's 10x10?", "100", count)
-count = question("6. What's 5x3?", "15", count) 
-count = question("7. What's 4x7?", "28", count) 
-count = question("8. What's 8x9?", "72", count)
-count = question("9. What's 6x4?", "24", count) 
-count = question("10. What's 1x0?", "0", count)
-print(f"Congratulations {username}! Your final score is", count)
+questions = ["2+3","5x5","3+200", "7x7", "36/4", "40x25", "55+60", "32+40"]
+answers = ["5", "25", "203", "49", "9", "1000", "115", "72"]
+for _ in range (5):
+    questionnumber = random.randint(0,7)
+    count = question(f"What is {questions[questionnumber]}? ",answers[questionnumber], count)
+#count = question("2. What's 9x4?", "36", count)  
+#count = question("1. What's 3x3?", "9", count)
+#count = question("3. What's 6x6?", "36", count) 
+#count = question("4. What's 7x7?", "49", count) 
+#count = question("5. What's 10x10?", "100", count)
+#count = question("6. What's 5x3?", "15", count) 
+#count = question("7. What's 4x7?", "28", count) 
+#count = question("8. What's 8x9?", "72", count)
+#count = question("9. What's 6x4?", "24", count) 
+#count = question("10. What's 1x0?", "0", count)
+if count >= 3:
+    print(f"Congratulations {username}! Your final score is", count)
+else:
+    print(f"Better luck next time {username} Your score is", count)
