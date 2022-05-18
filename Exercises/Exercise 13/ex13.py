1977de8ccf7442ce1f1659f98fcbb3707091950f
Konrad Witak - Disking IT Support KonradWitak-DiskingI Web Services <konrad@disking.co.uk>
2022-05-18 21:45:57 +0100

    meh

---
+++ Exercises/Exercise 13/ex13.py
@@ -0,0 +1,8 @@
+from sys import argv
+# read the WYSS section for how to run this
+script, orange, grapefruit, kiwi = argv #skip the script variable when running
+
+print("The script is called:", script)
+print("Your first variable is:", orange)
+print("Your second variable is:", grapefruit)
+print("Your third variable is:", kiwi)
